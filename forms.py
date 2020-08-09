8f811cd18f6488d92fee7907214162bb423e9ae7
Piotr Nowak <pinotestpiotrnowak@gmail.com>
2020-08-09 10:17:22 +0200

    Add forms.py file

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired
+
+
+class ExpenseForm(FlaskForm):
+    id = IntegerField('id', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    koszt = IntegerField('Koszt')
