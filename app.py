05750a6ae5bc3c649d82929c7184c1d9623a2590
Piotr Nowak <pinotestpiotrnowak@gmail.com>
2020-08-14 07:35:27 +0200

    Fix problem with id listed on main page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, abort, make_response, request, render_template
 from forms import ExpenseForm
 from models import expenses
 import logging
+import requests
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 app = Flask(__name__)
@@ -33,6 +34,7 @@ def bad_request(error):
 
 @app.route("/api/v1/expenses/", methods=["POST"])
 def create_expense():
+    # wyodrebnic całą funkcje
     if not request.json or not 'title' in request.json:
         abort(400)
     expense = {
@@ -68,6 +70,7 @@ def update_expense(expense_id):
     ]):
         abort(400)
     expense = {
+        'id': expense_id,
         'title': data.get('title', expense['title']),
         'description': data.get('description', expense['description']),
         'koszt': data.get('koszt', expense['koszt'])
@@ -84,24 +87,36 @@ def expenses_list():
     form = ExpenseForm()
     error = ""
     if request.method == "POST":
+        # create_expense() dopisac
         if form.validate_on_submit():
-
-            # expenses.create(form.data)
-            expenses.save_all()
+            response = requests.post(str(request.url_root) + url_for("create_expense"),
+                                     json=form.data, headers={'Content-type': 'application/json',
+                                                              'Accept': 'text/plain'})
+            logging.info("response code: %s" % response.status_code)
+            if response.status_code != 201:
+                logging.error("Technical problem with API funcion")
         return redirect(url_for("expenses_list"))
-
     return render_template("expenses.html", form=form, expenses=expenses.all(), error=error)
 
 
-@app.route("/expenses/<int:expense_id>/", methods=["GET", "PUT"])
+@app.route("/expenses/<int:expense_id>/", methods=["GET", "POST"])
 def expense_details(expense_id):
     expense = expenses.get(expense_id)
     form = ExpenseForm(data=expense)
 
-    if request.method == "PUT":
+    if request.method == "POST":
         if form.validate_on_submit():
-            expenses.update(expense_id - 1, form.data)
-        return redirect(url_for("expenses_list"))
+            full_url = str(request.url_root) + \
+                url_for("update_expense", expense_id=expense_id)
+            logging.info("full_url %s" % full_url)
+            response = requests.put(full_url,
+                                    json=form.data, headers={'Content-type': 'application/json',
+                                                             'Accept': 'text/plain'})
+            logging.info("response code: %s" % response.status_code)
+            if response.status_code != 201:
+                logging.error("Technical problem with API funcion")
+            # expenses.update(expense_id - 1, form.data)
+            return redirect(url_for("expenses_list"))
     return render_template("expense.html", form=form, expense_id=expense_id)
 
 
